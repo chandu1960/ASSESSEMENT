ff9c3a7d534a6d75b37c659d5ce8478dd615ee48
chandu1960 <86677069+chandu1960@users.noreply.github.com>
2024-03-30 23:54:58 +0530

    Update palindrome.rs

--- SUBMIT/palindrome.rs
+++ SUBMIT/palindrome.rs
@@ -1,1 +1,10 @@
+fn is_palindrome(s: &str) -> bool {
+    let s = s.to_lowercase();
+    let s = s.chars().filter(|c| c.is_alphanumeric()).collect::<String>();
+    s == s.chars().rev().collect::<String>()
+}
 
+fn main() {
+    let input = "A man, a plan, a canal, Panama!";
+    println!("Is '{}' a palindrome? {}", input, is_palindrome(input));
+}
